a6395d45d64adf53cce6f7bb02f933c9b8ecbef3
SalahSalah008 <salah.salah5540@coyote.csusb.edu>
2024-01-30 14:10:31 -0800

    Hello First project

---
+++ Lab 1/Hello.cpp
@@ -0,0 +1,13 @@
+/***
+*{Short description of the lab}
+***/
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    printf("Hello World again!\n");
+    return 0;
+}
+
