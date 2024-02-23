b02312b0b6380a8d9c248ed8a272ef5621a04596
SalahSalah008 <salah.salah5540@coyote.csusb.edu>
2024-02-22 23:51:20 -0800

    update

--- Lab 4/Lab4_func.cpp
+++ Lab 4/Lab4_func.cpp
@@ -23,7 +23,7 @@ int sumOfThree(int x, int y, int z){
 double division(double x, double y){
     return x / y;
 }
-/** isGreatherThan
+/** isGreaterThan
 * @brief returns whether x is greater than y
 *
 * @param x : (double)
@@ -31,7 +31,7 @@ double division(double x, double y){
 * @return true : x is greater than y
 * @return false : x is less than or equal to y
 */
-bool isGreatherThan(double x, int y){
+bool isGreaterThan(double x, int y){
     return x > y;
 }
 /** select
@@ -65,4 +65,28 @@ string select(string func_type){
 template <typename T>
 void myPrint(string func_type, T output){
     cout << select (func_type) << output << endl;
+}
+int main(){
+    // initialtize 3 int parameters
+    int x1 = 2;
+    int x2 = 3;
+    int x3 = 5;
+
+    //call sum of three 
+    int sum_out = sumOfThree(x1, x2, x3);
+    // call my print
+    myPrint("sum", sum_out);
+
+    //initizlize 2 double parameters
+    double dividend = 12345.1;
+    double divisor = 2345.6;
+
+    // call division 
+    double div_out = division(dividend, divisor);
+    myPrint("division", div_out);
+
+    //compare the div_out and sum_out
+    bool is_greater_out = isGreaterThan(div_out, sum_out);
+    myPrint("comprison", is_greater_out);
+    return 0;
 }
