add607ddee295bc0882612832cb0699abda2501e
SalahSalah008 <salah.salah5540@coyote.csusb.edu>
2024-05-13 20:08:28 -0700

    f

---
+++ Lab 11/Charmander.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <cstdio>
+#include "Charmander.h"
+using namespace std;
+
+/**
+ * @brief Construct a new Charmander:: Charmander object
+ */
+Charmander::Charmander() : Pokemon() {
+    type.push_back("Fire");
+    skills.push_back("Growl");
+    skills.push_back("Scratch");
+    cout << "Default Constructor (Charmander)\n";
+}
+
+/**
+ * @brief Construct a new Charmander:: Charmander object
+ *
+ * @param name 
+ * @param hp 
+ * @param att 
+ * @param def 
+ * @param t 
+ * @param s 
+ */
+Charmander::Charmander(string name, int hp, int att, int def, vector<string> t, vector<string> s)
+    : Pokemon(name, hp, att, def, t) {
+    skills = s;
+    cout << "Overloaded Constructor (Charmander)\n";
+}
+
+/**
+ * @brief says what a charmander says
+ */
+void Charmander::speak() {
+    cout << "Charmander-char\n";
+}
+
+/**
+ * @brief calls printStats from the parent class (Pokemon) and appends the skills used for Charmander
+ */
+void Charmander::printStats() {
+    Pokemon::printStats();
+    cout << "Skills: ";
+    for(int i = 0; i < skills.size(); i++) {
+        cout << skills[i] << "\t";
+    }
+    cout << endl;
+}
